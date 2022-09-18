d57b603539faa49851560818bc5995562ade314b
sameh-dh <samehdh44@gmail.com>
2022-09-18 17:31:34 +0100

    basic css

--- timer/src/App.js
+++ timer/src/App.js
@@ -2,46 +2,95 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
   const [seconds,setSeconds]=useState(0);
   const [minutes,setMinutes]=useState(0);
- var timer;
+  const [change,setChange]=useState(true);
+  var timer;
   useEffect(() => {
-  timer=setInterval(() => {
-   setSeconds(seconds+1);
+    setChange(false);
 
+  timer=setInterval(() => {
+    setChange(true);
+  setSeconds(seconds+1);
+  
+  // from seconds to minutes
     if(seconds===59){
       setMinutes(minutes+1);
+      
+     
       setSeconds(0);
     }
    
   },1000)
-  return () => clearInterval(timer);
-  })
+ 
+  return () =>{     
 
+    clearInterval(timer)};
+  })
+// Restart counter
   const restart=()=>{
       setSeconds(0);
       setMinutes(0);
   }
-  const stop =()=>{
-   clearInterval(timer);
-  }
-
-
+  
+  const animation1 = change ? 'fold' : 'unfold';
+  const animation2 = change ? 'fold' : 'unfold';
   return (
   
     <div>
   	<div className="timer">
     <div className='container'>
-      <div className='timer-container'>
-        <h1>Timer</h1>
-       
-       <h1>{minutes<10 ? "0"+minutes:minutes}:{seconds<10 ?"0"+seconds:seconds}</h1>
+    <div className='timer-container'>
+      <h1>Timer</h1>
+      <h1>{minutes<10 ? "0"+minutes:minutes}:{seconds<10 ?"0"+seconds:seconds}</h1>
+
       <button onClick={restart}>Restart</button>
-      <button onClick={stop}>Stop</button>
-      </div>
-    </div>
 
     </div>
+
+
+    <div className={'flipCounter'}>
+				<div className={'upperCard'}>
+					<span>{seconds<10 ?"0"+seconds:seconds}</span>
+				</div>
+				<div className={'lowerCard'}>
+					<span>{seconds<10 ?"0"+seconds:seconds}</span>
+				</div>
+				<div className={`flipCard first ${animation1}`}>
+					<span>{seconds<10 ?"0"+seconds:seconds}</span>
+				</div>
+        <div className={`flipCard first ${animation2}`}>
+					<span>{seconds<10 ?"0"+seconds:seconds}</span>
+				</div>
+			</div>
+
+
+      
+    </div>
+
+
+
+
+    <div className={'flipCounter'}>
+				<div className={'upperCard'}>
+					<span>{minutes<10 ? "0"+minutes:minutes}</span>
+				</div>
+				<div className={'lowerCard'}>
+					<span>{minutes<10 ? "0"+minutes:minutes}</span>
+				</div>
+				<div className={`flipCard first`}>
+					<span>{minutes<10 ? "0"+minutes:minutes}</span>
+				</div>
+        <div className={`flipCard first `}>
+					<span>{minutes<10 ? "0"+minutes:minutes}</span>
+				</div>
+			</div>
+
+    </div>
+
+
+
     </div>
   );
 }
