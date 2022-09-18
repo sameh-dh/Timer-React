95e042c1a28553c75ba598d6fa65fbfd1bbf45d3
sameh-dh <samehdh44@gmail.com>
2022-09-18 15:16:51 +0100

    timer count up is functionnel

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,12 +1,47 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [seconds,setSeconds]=useState(0);
+  const [minutes,setMinutes]=useState(0);
+ var timer;
+  useEffect(() => {
+  timer=setInterval(() => {
+   setSeconds(seconds+1);
+
+    if(seconds===59){
+      setMinutes(minutes+1);
+      setSeconds(0);
+    }
+   
+  },1000)
+  return () => clearInterval(timer);
+  })
+
+  const restart=()=>{
+      setSeconds(0);
+      setMinutes(0);
+  }
+  const stop =()=>{
+   clearInterval(timer);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-      
+  
+    <div>
+  	<div className="timer">
+    <div className='container'>
+      <div className='timer-container'>
+        <h1>Timer</h1>
        
-      </header>
+       <h1>{minutes<10 ? "0"+minutes:minutes}:{seconds<10 ?"0"+seconds:seconds}</h1>
+      <button onClick={restart}>Restart</button>
+      <button onClick={stop}>Stop</button>
+      </div>
+    </div>
+
+    </div>
     </div>
   );
 }
